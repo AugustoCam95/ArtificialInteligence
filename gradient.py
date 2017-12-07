e82f2880c4414191adeed034584ca4d9b4f5fd54
José Augusto <augustocam95@gmail.com>
2017-12-07 11:27:29 -0300

    Complete

--- gradient.py
+++ gradient.py
@@ -1,17 +1,9 @@
 import numpy as np
-
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
-from sympy import *
-init_printing()
-x,y = symbols('x y')
-def f(x,y): return ((4-((2.1)*(x**2))+((1/3)*(x**4)))*(x**2))+(x*y)-((4*(1-(y**2)))*(y**2))
-def gx(x,y): return (8*x-8.4*(x**3)+2*(x**5)+y)
-def gy(x,y): return (x+16*(y**3)-8*y)
-def hesssx(x,y): return	(8-25.2*(x**2)+10*(x**4))
-def hesssy(x,y): return (48*(y**2)-8)
+
 
 def fun(x,y):
     return ((4-((2.1)*(x**2))+((1/3)*(x**4)))*(x**2))+(x*y)-((4*(1-(y**2)))*(y**2))
@@ -28,19 +20,7 @@ def hessxx(x):
 def hessyy(y):
 	return 48*(y**2)-8
 
-print("Função a ser otimizada:")
-print(f(x,y))
-print("\n")
 
-gradiente = np.array([gx(x,y), gy(x,y)])
-hessiana = np.array([[hesssx(x,y), 1.],
-					 [1., hesssy(x,y)]])
-
-print("Vetor gradiente:")
-print(gradiente)
-print("\n")
-print("Matriz hessiana:")
-print(hessiana)
 
 print("\n")
 print("-------------Método gradiente---------------")
@@ -55,9 +35,11 @@ iterações = 0 #iterações inicializa com zero.
 p = np.array([gradx(b,c),grady(b,c)])
 old = np.array([b,c])
 d = -p
+viter = [old]
 
 while True:
 	new = old + a*d
+	viter.append(new)
 	err = np.abs(fun(old[0],old[1])-fun(new[0],new[1]))
 	iterações = iterações + 1
 	print("passo:",iterações)
@@ -73,13 +55,27 @@ while True:
 		break;
 
 
-eixox = np.arange(-5,5, 0.25)
-eixoy = np.arange(-5,5, 0.25)
-eixox,eixoy = np.meshgrid(eixox,eixoy)
+xx = []
+yy = []
+z = []
+
+for i in range(len(viter)):
+	xx.append(viter[i][0])
+	yy.append(viter[i][1])
+	z.append(fun(viter[i][0],viter[i][1]))
+
+x = np.arange(-3,3, 0.25)
+y = np.arange(-2,2, 0.25)
+x,y = np.meshgrid(x,y)
 function = ((4-((2.1)*(x**2))+((1/3)*(x**4)))*(x**2))+(x*y)-((4*(1-(y**2)))*(y**2))
 
 fig = plt.figure()
 ax = Axes3D(fig)
-ax.plot_surface(eixox,eixoy,function, rstride = 1, cstride = 1, cmap=cm.viridis)
+ax.plot_surface(x,y,function, rstride = 1, cstride = 1, cmap=cm.viridis)
+ax.plot(xx,yy,z)
+ax.scatter(xx,yy,z, c = 'r', marker = 'o', alpha = 1)
+ax.set_xlabel("x1")
+ax.set_ylabel("x2")
+ax.set_zlabel("f(x1,x2)")
 
 plt.show()
